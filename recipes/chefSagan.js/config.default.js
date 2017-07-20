714ff544f256c9f7d4ea4fe62b467ed48596f9cc
Jonathan Clow <jonathan.clow@ibm.com>
2017-07-20 09:45:11 -0500

    Updated package to support code style

--- recipes/chefSagan.js/config.default.js
+++ recipes/chefSagan.js/config.default.js
@@ -16,20 +16,20 @@ exports.credentials = {};
 // Watson Conversation
 // https://www.ibm.com/watson/developercloud/conversation.html
 exports.credentials.conversation = {
-	password: '',
-	username: ''
+  password: '',
+  username: '',
 };
 
 // Watson Speech to Text
 // https://www.ibm.com/watson/developercloud/speech-to-text.html
 exports.credentials.speech_to_text = {
-	password: '',
-	username: ''
+  password: '',
+  username: '',
 };
 
 // Watson Text to Speech
 // https://www.ibm.com/watson/developercloud/text-to-speech.html
 exports.credentials.text_to_speech = {
-	password: '',
-	username: ''
+  password: '',
+  username: '',
 };
